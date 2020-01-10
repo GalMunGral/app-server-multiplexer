8ea384f3708811e2ab8de3a7e57df7d5fe8bcab0
Wenqi He <hewenqi96@gmail.com>
2020-01-10 06:56:48 -0500

    Update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,22 @@
 const excludedPaths = new Set(['/', '/index.html'])
 let origin;
 self.addEventListener('message', e => {
-  console.log('Origin: ', origin);
   origin = e.data;
 })
 self.addEventListener('fetch', e => {
   let url = new URL(e.request.url);
-  console.log('Intercepted request for:', url.host, url.pathname);
+  let path;
   if (url.host !== location.host) {
-    e.respondWith(fetch('/proxy/' + encodeURIComponent(url.href)));
+    path = encodeURIComponent(url.href)
   } else if (!excludedPaths.has(url.pathname) && origin) {
-    e.respondWith(fetch('/proxy/' + encodeURIComponent(origin + url.pathname)));
+    path = encodeURIComponent(origin + url.pathname);
+  }
+  if (path) {
+    e.respondWith(
+      fetch('/proxy/' + path, {
+        method: e.request.method,
+        headers: e.request.headers
+      })
+    );
   }
 });
--- server.js
+++ server.js
@@ -7,15 +7,19 @@ app.get('/proxy/:url', async (req, res) => {
   const fetchURL = encodeURI(decodeURIComponent(req.params.url));
   // console.log('Fetching', url)
   try {
-    const response = await fetch(fetchURL, { timeout: 1000 });
+    const response = await fetch(fetchURL, {
+      headers: {
+        Range: req.headers.range
+      },
+      timeout: 5000
+    });
     res.set('Content-Type', response.headers.get('Content-Type'));
-    console.log(response.headers.get('Content-Type'));
+    res.status(response.status);
     let type;
-    if ((type = response.headers.get('Content-Type')) && type.includes('text')) {
+    if ((type = response.headers.get('Content-Type')) && (type.includes('text') || type.includes('javascript'))) {
       let text = await response.text();
       const url = new URL(req.params.url);
-      console.log(url.origin, url.host);
-      text = text.replace(new RegExp(url.origin, 'g'), 'http://localhost:8080');
+      text = text.replace(new RegExp(url.host, 'g'), 'localhost:8080');
       res.send(text);
     } else {
       response.body.pipe(res);  
